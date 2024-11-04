737c43dfc9734f0e3c2360af3f9fb87fa131ec7b
taoning <taoning.leon@bytedance.com>
2024-11-04 11:54:14 +0800

    feat: add bufferline keymap

--- lua/keybinding.lua
+++ lua/keybinding.lua
@@ -1,4 +1,14 @@
+local opt = {
+	silent = true,
+	remap = false
+}
+
+-- move
 vim.keymap.set('v', 'K', ":move '<-2<CR>gv-gv", { silent = true, remap = false })
 vim.keymap.set('v','J', ":move '>+1<CR>gv-gv", { silent = true, remap = false })
 vim.keymap.set('v', '>', '>gv', { silent = true, remap = false })
 vim.keymap.set('v', '<', '<gv', { silent = true, remap = false })
+
+-- bufferline
+vim.keymap.set('n', '<s-h>', '<cmd>BufferLineCyclePrev<cr>', opt)
+vim.keymap.set('n', '<s-l>', '<cmd>BufferLineCycleNext<cr>', opt)
