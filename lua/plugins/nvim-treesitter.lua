324318415ab74278a9fa8442cc4f315403928367
taoning <taoning.leon@bytedance.com>
2024-11-04 14:13:38 +0800

    feat: add java to treesitter

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -10,6 +10,7 @@ local function treesister_config()
     require 'nvim-treesitter.configs'.setup({
         -- A list of parser names, or "all" (the four listed parsers should always be installed)
         ensure_installed = {
+			"java",
             "go",
             "lua",
             "vim",
