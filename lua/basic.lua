5041662e895883193fde40e4a9579c539bff2522
taoning <taoning.leon@bytedance.com>
2024-11-15 05:28:19 +0800

    modify basic info

--- lua/basic.lua
+++ lua/basic.lua
@@ -1,13 +1,11 @@
 -- tabstop 4 space
 vim.o.tabstop = 4
-vim.bo.tabstop = 4
 vim.o.softtabstop = 4
-vim.bo.shiftwidth = 4
 vim.o.shiftwidth = 4
 
 -- 使用相对行号
 vim.wo.number = true
-vim.wo.relativenumber = true
+-- vim.wo.relativenumber = true
 
 -- 高亮所在行
 vim.wo.cursorline = true
